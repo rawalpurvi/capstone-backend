27e8975339a69e265485e56a4b48faae9f3226b5
Purvi <purvirawal.work@gmail.com>
2021-04-14 16:32:56 -0700

    chore: Change port in app.py

--- app.py
+++ app.py
@@ -309,4 +309,4 @@ def create_app(test_config=None):
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=os.environ['PORT'], debug=True)
